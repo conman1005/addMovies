c83aa4d181b27aaac9a5ff9c656e1fc85a19bc9b
Resonantbell <96964149+Resonantbell@users.noreply.github.com>
2023-02-11 20:36:41 -0500

    Updated theme of program to user book_list

--- Lab2_11.py
+++ Lab2_11.py
@@ -7,18 +7,19 @@ import csv
 #import os to enable clearing of the console
 import os
 
+
 #initially clear console to get rid of top text from program opening
 os.system('cls')
 
 #initially read file
 header = []
 rows = []
-with open('movies.csv', newline='') as file:
+with open('book_list.csv', newline='') as file:
     reader = csv.reader(file)
     #get csv header
     
     header = next(reader)
-    header
+    # header
     #get rows
     for row in reader:
         rows.append(row)
@@ -26,48 +27,48 @@ with open('movies.csv', newline='') as file:
 
 #loop program
 loop = True
-print("The commands for this program are 'addMovie', 'printMovies', 'searchMovie' and 'exit'\n")
+print("The commands for this program are: 'add book', 'print books', 'search books' and 'exit'\n")
 while (loop):
     #Prompt for command
     command = input("Enter Command: ")
     #exit program if user enters 'exit'
     if command == "exit":
         loop=False
-    elif command == "addMovie":
+    elif command == "add book":
         #clear console
         os.system('cls')
-        #Prompt for new movie details
-        newName = input("Type movie name to Add: ")
-        newDirector = input("Type the Director for " + newName + ": ")
+        #Prompt for new book details
+        newTitle = input("Type book title to add: ")
+        newAuthor = input("Enter the author of " + newTitle + ": ")
         isNumber = False
         newYear=""
         #loop for number input
         while isNumber == False:
-            #prompt for movie year
+            #prompt for book year
             newYear = input("Please enter the release year: ")
             try :
                 #checks if string can be converted to integer, no need to actually convert to integer
                 int(newYear)
                 #if the error doesn't happen, you get to this part, setting inNumber to True and ending loop
                 isNumber = True
-                print("\nMovie Successfully added.\n")
+                print("\nBook Successfully added.\n")
             except ValueError :
                 #Prompts for proper input if input ins't a number
-                print("Please enter a Number for the Year.\n")
+                print("Try again, please enter a Number for the Year.\n")
 
         #append new data
-        rows.append([newName,newDirector,newYear])
+        rows.append([newTitle,newAuthor,newYear])
 
-        with open('movies.csv', 'w', newline='') as file:
+        with open('book_list.csv', 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["title","director","year"])
+            writer.writerow(["title","author","year"])
             for row in rows:
                 writer.writerow(row)
             file.close
-    elif command == "printMovies":
+    elif command == "print books":
         #clear console
         os.system('cls')
-        with open('movies.csv', newline='') as file:
+        with open('book_list.csv', newline='') as file:
             reader = csv.reader(file)
             #get csv header
             header = []
@@ -77,23 +78,23 @@ while (loop):
             rows = []
             for row in reader:
                 rows.append(row)
-                print(row[0] + " was Directed by " + row[1] + " and was released in " + row[2] + "\n")
+                print(row[0] + " was written by " + row[1] + " and was released in " + row[2] + "\n")
             file.close
-    elif command == "searchMovie":
+    elif command == "search books":
         #clear console
         os.system('cls')
-        #prompt for movie title
-        search = input("Enter Movie Title to Search: ")
+        #prompt for book title
+        search = input("Enter Book Title to Search: ")
         #boolean to let user know if the search found anything
         found = False
         #loop through each row and check if the title equals the search phrase.
         for row in rows:
             if row[0].lower() == search.lower():
                 #if the search phrase equals any title than the details will be printed.
-                print("\n" + row[0] + " was Directed by " + row[1] + " and was released in " + row[2] + "\n")
+                print("\n" + row[0] + " was written by " + row[1] + " and was released in " + row[2] + "\n")
                 found = True
         if found == False:
-            print("There is no movie in the database with the name " + search  + ", you can use the 'addMovie' command to add it.\n")
+            print("There is no book in the database with the name " + search  + ", you can use the 'addBook' command to add it.\n")
     else:
         os.system('cls')
-        print("Please enter a valid command. The commands for this program are 'addMovie', 'printMovies', 'searchMovie' and 'exit'\n")
+        print("Please enter a valid command. The commands for this program are 'addbook', 'printbooks', 'searchbooks' and 'exit'\n")
